27cffbbb9516d6c9aeb37bc2ae670cbc1c1f0d5a
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-11-03 10:40:46 -0700

    use LOGGER_NAME variable for SESSION_FILE and COOKIE_EXP_FILE strings

--- pensando_psm/constants.py
+++ pensando_psm/constants.py
@@ -1,5 +1,5 @@
 LOGGER_NAME = 'pensando_psm'
 TMP_FILE_ROOT = '/tmp'
-SESSION_FILE = 'pensando_psm_state_session'
-COOKIE_EXP_FILE = 'pensando_psm_state_cookie_expiration'
+SESSION_FILE = f'{LOGGER_NAME}_state_session'
+COOKIE_EXP_FILE = f'{LOGGER_NAME}_state_cookie_expiration'
 SENTINEL_IP = '192.0.2.42'
